c9b7c3e6bed6334d8df158bf6c72791d21498a47
supercoelhao <fernandoazevedogeek@gmail.com>
2026-02-05 20:29:23 -0300

    ;

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BREAKINGBADREALFANS.COM</title>
+</head>
+<body>
+    
+</body>
+    <div class="container">
+    <?php
+    $titulo = "BREKING BAD REAL FANS";
+    $subti = "Say my name, bitch!";
+
+    echo "<h1> $titulo <br> $subti </h1>";
+    ?>
+    </div>
+
+</html>
